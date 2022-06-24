a49d5469c193d1345a42918eb814a442eb1a9383
jack2000-cpu <jasonmwangi2000@gmail.com>
2022-06-24 20:01:43 +0300

    Initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 // *** Write your code here***
+
+//console.log("up and running");
+
+//to get the display screen
+let screen = document.querySelector("#display")
+
+function calcNumbers(x){
+  screen.value += x;
+  console.log(x)
+  
+}
